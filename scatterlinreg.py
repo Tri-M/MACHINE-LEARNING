40ae4a86bf1991013ef360a9506bc05975d94e18
Trisha M <trisham0910@gmail.com>
2022-07-26 14:44:07 +0530

    -

---
+++ scatterlinreg.py
@@ -0,0 +1,38 @@
+import numpy as np
+from numpy.linalg import inv
+import matplotlib.pyplot as plt
+
+class LinearRegression:
+
+    def __init__(self):
+        pass
+       
+
+    def trainingset(self,X,Y):  
+        res=inv(X.T.dot(X)).dot(X.T).dot(Y)
+        return res
+
+    def testingset(self,X,param):
+        ans=X.dot(param)
+        return ans
+
+lr=LinearRegression()   
+data=np.array([[4,33],[4.5,42],[5,45],[5.5,51],[6,53],[6.5,61],[7,62]])
+
+X=data[:,0]
+y=data[:,1]
+
+print (X)
+print (y)
+xt=np.transpose(X)
+
+X=X.reshape(len(X),1)
+
+#print (X)
+res=lr.trainingset(X,y)
+print(res)
+yp=lr.testingset(X,res)
+#print (yp)
+plt.scatter(X,y)
+plt.plot(X,yp,"y")
+plt.show()
