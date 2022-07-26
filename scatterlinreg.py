3b363c1fbf17b14f49ab74eb4a1732745c8cd129
Trisha M <trisham0910@gmail.com>
2022-07-26 15:24:26 +0530

    ---

--- scatterlinreg.py
+++ scatterlinreg.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numpy.linalg import inv
+from numpy.linalg import *
 import matplotlib.pyplot as plt
 
 class LinearRegression:
@@ -22,6 +22,7 @@ data=np.array([[4,33],[4.5,42],[5,45],[5.5,51],[6,53],[6.5,61],[7,62]])
 X=data[:,0]
 y=data[:,1]
 
+
 print (X)
 print (y)
 xt=np.transpose(X)
@@ -32,7 +33,21 @@ X=X.reshape(len(X),1)
 res=lr.trainingset(X,y)
 print(res)
 yp=lr.testingset(X,res)
-#print (yp)
+print (yp)
 plt.scatter(X,y)
-plt.plot(X,yp,"y")
+plt.plot(X,yp,"b")
 plt.show()
+print("__")
+print(sum(X))
+print(sum(yp))
+sumofyp=sum(yp)
+ysum=sum(y)
+ypmean=np.mean(yp)
+print(ypmean)
+print(ysum)
+#print(sumofyp-ypmean)
+#print(sum(y)-ypmean)
+ymean=np.mean(y)
+print(pow(sum(yp-ymean),2))
+
+
