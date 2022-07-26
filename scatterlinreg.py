c5d94a28d67933fbc1543f5fa095a4c531ef65c1
Tri-M <trisham0910@gmail.com>
2022-07-26 17:53:32 +0530

    .

--- scatterlinreg.py
+++ scatterlinreg.py
@@ -1,53 +1,67 @@
-import numpy as np
-from numpy.linalg import *
+import numpy as np   
 import matplotlib.pyplot as plt
 
-class LinearRegression:
+soaps=[4, 4.5, 5, 5.5, 6, 6.5, 7]
+Y=sud=[33, 42, 45, 51, 53, 61, 62]
 
-    def __init__(self):
-        pass
-       
+p=plt.scatter(soaps,sud)
+Xt=[[1, 1, 1, 1, 1, 1, 1],soaps]
+print(Xt)
 
-    def trainingset(self,X,Y):  
-        res=inv(X.T.dot(X)).dot(X.T).dot(Y)
-        return res
-
-    def testingset(self,X,param):
-        ans=X.dot(param)
-        return ans
-
-lr=LinearRegression()   
-data=np.array([[4,33],[4.5,42],[5,45],[5.5,51],[6,53],[6.5,61],[7,62]])
-
-X=data[:,0]
-y=data[:,1]
+print("\nX^T: ")
+X=np.transpose(Xt)
+print(X)
 
 
-print (X)
-print (y)
-xt=np.transpose(X)
-
-X=X.reshape(len(X),1)
-
-#print (X)
-res=lr.trainingset(X,y)
-print(res)
-yp=lr.testingset(X,res)
-print (yp)
-plt.scatter(X,y)
-plt.plot(X,yp,"b")
-plt.show()
-print("__")
-print(sum(X))
-print(sum(yp))
-sumofyp=sum(yp)
-ysum=sum(y)
-ypmean=np.mean(yp)
-print(ypmean)
-print(ysum)
-#print(sumofyp-ypmean)
-#print(sum(y)-ypmean)
-ymean=np.mean(y)
-print(pow(sum(yp-ymean),2))
+print("\nX^T X: ")
+XtX=np.dot(Xt,X)
+print(XtX)
 
 
+print("\nX^T X^-1: ")
+XtXi=np.linalg.inv(XtX)
+print(XtXi)
+
+
+print("\nX^TY")
+XtY=np.dot(Xt,Y)
+print(XtY)
+
+
+print("\nB0 B1")
+B=np.dot(XtXi,XtY)
+print(B)
+
+print("\nLinear equation : ")
+if (B[0]<0):
+    print("y="+str(B[1])+"x"+str(B[0]))
+else:
+    print("y="+str(B[1])+"x+"+str(B[0]))
+
+
+x = np.linspace(0,7)
+y = B[1]*x+B[0]
+plt.plot(x, y, '-0', label='y=2x+1')
+
+SSE=0
+SE=0
+for i in range(len(sud)):
+    SE+=sud[i]-(B[1]*soaps[i]+B[0])
+    SSE+=(sud[i]-(B[1]*soaps[i]+B[0]))**2 #Quantify the error
+print("Errors sum: "+str(SE)+"\nSSE: "+str(SSE))
+
+Y_ = np.mean(Y)
+print("\nMean (Y_): "+str(Y_))
+
+S=0
+for i in sud:
+    S+=i-Y_
+print("Sum of Yi-Y_: "+str(S))
+
+SST=0
+for i in sud:
+    SST+=(i-Y_)**2
+print("SST: "+str(SST))
+
+SSR=SST-SSE
+print("SSR: "+str(SSR))
