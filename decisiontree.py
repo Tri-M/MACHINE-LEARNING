8a03f66d0e1471aae539abe89a9fa16ed4258c05
Trisha M <trisham0910@gmail.com>
2022-10-18 14:59:10 +0530

    Update decisiontree.py

--- decisiontree.py
+++ decisiontree.py
@@ -1,39 +1,61 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import classification_report
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-
-df = pd.read_csv("tic-tac-toe-endgame.csv")
-print(df)
-labels= preprocessing.LabelEncoder()                     
-df=df.apply(labels.fit_transform) 
-print(df)
-feature_cols = ['V1','V2','V3','V4','V5','V6','V7','V8','V9']
-X = df[feature_cols]                               
-y = df.V10
-print("X :",X)
-print("y:" ,y)
-X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2)
-classifier =DecisionTreeClassifier(criterion="entropy", random_state=100)     
-classf_gini=DecisionTreeClassifier(criterion="gini",random_state=100)
-classf=classf_gini.fit(X_train,Y_train)
-
-classifier.fit(X, y)    
+from sklearn import tree
+import matplotlib.pyplot as plt
 
 
-y_pred= classifier.predict(X)  
-y_pred1=classf_gini.predict(X)
-print(y_pred)
-print("Accuracy score :",accuracy_score(y,y_pred))
-print("Gini index (accuracy) : ",accuracy_score(y,y_pred1))
-print("confusion matrix :",confusion_matrix(y,y_pred))
 
-    
-    
+df = pd.read_csv('tic-tac-toe-endgame.csv')
 
+one_hot_encoded_data = pd.get_dummies(df, columns = ['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9'])
 
+features = list(one_hot_encoded_data.columns)[1:]
+
+train, test = train_test_split(one_hot_encoded_data, test_size = 0.2)
+
+trainSize = len(train)
+testSize = len(test)
+
+X = train[features]
+
+Y = train['V10']
+
+dtree = DecisionTreeClassifier()
+dtree = dtree.fit(X, Y)
+
+plt.figure(figsize=(50, 30))
+
+tree.plot_tree(dtree, feature_names=features)
+
+plt.savefig("output.png")
+
+plt.show()
+
+prediction = dtree.predict(X)
+
+Yarr = np.array(Y)
+
+trainingError = 0
+
+for i in range(trainSize):
+    if prediction[i] != Yarr[i]:
+        trainingError += 1
+
+print("Training error: ", trainingError)
+
+Ypred = dtree.predict(test[features])
+
+testError = 0
+
+Ytest = np.array(test['V10'])
+
+for i in range(testSize):
+    if Ypred[i] != Ytest[i]:
+        testError += 1
+
+print("Testing Error: ", testError)
+
+print("Height: ", dtree.get_depth()+1)
