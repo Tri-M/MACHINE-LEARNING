98cad49b765fbcd5e8fb7ede612dfb4b64a8c25e
Trisha M <trisham0910@gmail.com>
2022-10-26 09:53:19 +0530

    Add files via upload

---
+++ kmeanscluster.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns
+from sklearn.cluster import KMeans 
+from sklearn.metrics import silhouette_score
+from sklearn.preprocessing import MinMaxScaler
+
+from sklearn.cluster import KMeans
+
+iris=pd.read_csv("Iris.csv")
+print(iris)
+x = iris.iloc[:, [0, 1, 2, 3]].values
+
+print(iris.info())
+iris_setosa=iris.loc[iris["Species"]=="Iris-setosa"]
+iris_virginica=iris.loc[iris["Species"]=="Iris-virginica"]
+iris_versicolor=iris.loc[iris["Species"]=="Iris-versicolor"]
+
+
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+    kmeans.fit(x)
+  
+plt.xlabel('Number of clusters')
+
+
+kmeans = KMeans(n_clusters = 3, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+y_kmeans = kmeans.fit_predict(x)
+plt.scatter(x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Iris-setosa')
+plt.scatter(x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], s = 100, c = 'orange', label = 'Iris-versicolour')
+plt.scatter(x[y_kmeans == 2, 0], x[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Iris-virginica')
+
+
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:,1], s = 100, c = 'black', label = 'Centroids')
+
+plt.legend()
+
+
+
+
