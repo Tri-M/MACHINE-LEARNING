ea37579d6811869dc446a42ec8fba216f24341e3
Trisha M <trisham0910@gmail.com>
2022-10-27 18:58:12 +0530

    knn

--- knn.py
+++ knn.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-from sklearn.datasets import load_breast_cancer
 from sklearn.metrics import confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
